026c88388ce31f613297eb2373b1f4c195096956
mac <mac@MacBook-Pro-de-mac.local>
2025-02-20 16:39:55 +0100

    remove all

--- src/main/java/com/example/springbatch_test1/repo/AcademicEmailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbatch_test1.repo;
-
-import com.example.springbatch_test1.entity.AcademicEmail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcademicEmailRepository extends JpaRepository<AcademicEmail, Integer> {
-}
